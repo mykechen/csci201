edb9cb2c96023fa7c68759a1be6b8c29719ab136
Myke Chen <149104012+mykechen@users.noreply.github.com>
2025-04-29 11:14:12 -0700

    created setup.sql schema/DB

---
+++ setup.sql
@@ -0,0 +1,31 @@
+-- Create schema
+CREATE SCHEMA trojanbites;
+USE trojanbites;
+
+-- USER table
+CREATE TABLE User (
+    user_id VARCHAR(20) PRIMARY KEY,
+    password VARCHAR(40)
+);
+
+-- RECIPE table
+CREATE TABLE Recipe (
+    recipe_id INT PRIMARY KEY,
+    user_id VARCHAR(20),
+    title VARCHAR(100),
+    tags TEXT,
+    description TEXT,
+    ingredients JSON,
+    instructions TEXT,
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+-- VOTING table
+CREATE TABLE Voting (
+    user_id VARCHAR(20),
+    recipe_id INT,
+    timestamp TIMESTAMP,
+    PRIMARY KEY (user_id, recipe_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id)
+);
