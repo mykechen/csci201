b33224d50ad0a87617781cedae34a25846e069e8
Ai Vy Nguyen <aivynngu@usc.edu>
2025-04-29 21:03:42 -0700

    renamed file, implementation of /signup & /login
    - still need to figure out password hashing in order to complete implementation
    - login partially implemented

---
+++ backend/src/main/java/com/team10/demo/controller/AuthenticationController.java
@@ -0,0 +1,142 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+@RestController
+public class AuthenticationController {
+
+    private static final String SQLurl = ""; // Change
+    private static final String SQLuser = ""; // Change
+    private static final String SQLpassword = ""; // Change
+
+    private static final String domain = "@usc.edu";
+
+    public AuthenticationController () {
+
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody User request) {
+
+        String email = request.getUserEmail();
+        String password = request.getUserPass();
+
+        Connection conn = null;
+        Statement st = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        //user domain is not @usc.edu
+        if (!email.endsWith(domain)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please use a " + domain + " email address.");
+        }
+
+        try{
+
+            //check if user already exists
+            conn = DriverManager.getConnection(SQLurl, SQLuser, SQLpassword);
+            st = conn.createStatement();
+            ps = conn.prepareStatement("SELECT * from `User` WHERE user_id = ?");
+            ps.setString(1, email);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("User with this email address already exists.");
+            }
+
+            //else: register user to database + hash password
+            //Pbkdf2PasswordEncoder pwEncoder = new Pbkdf2PasswordEncoder();
+
+            ps = conn.prepareStatement("INSERT INTO `User` (user_id, password) VALUES (?, ?)");
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ps.executeUpdate();
+
+            return ResponseEntity.status(HttpStatus.CREATED).body("Successfully registered user");
+
+        } catch (SQLException sqle) {
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Database error: " + sqle.getMessage());
+
+        } finally {
+
+            try {
+
+                if (rs != null) { rs.close(); }
+                if (st != null) { st.close(); }
+                if (ps != null) { ps.close();}
+                if (conn != null) { conn.close(); }
+
+            } catch (SQLException sqle) {
+
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Database error: " + sqle.getMessage());
+
+            }
+        }
+
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User request) {
+
+        String email = request.getUserEmail();
+        String password = request.getUserPass();
+
+        Connection conn = null;
+        Statement st = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DriverManager.getConnection(SQLurl, SQLuser, SQLpassword);
+            st = conn.createStatement();
+
+            //check for email in database
+            ps = conn.prepareStatement("SELECT * from `User` WHERE user_id = ?");
+            ps.setString(1, email);
+            rs = ps.executeQuery();
+
+            if (!rs.next()) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found");
+            }
+
+            //check for correct password
+
+            return ResponseEntity.status(HttpStatus.OK).body("User successfully logged in");
+
+        } catch (SQLException sqle) {
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Database error: " + sqle.getMessage());
+        
+        } finally {
+
+            try {
+
+                if (rs != null) { rs.close(); }
+                if (st != null) { st.close(); }
+                if (ps != null) { ps.close();}
+                if (conn != null) { conn.close(); }
+
+            } catch (SQLException sqle) {
+
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Database error: " + sqle.getMessage());
+
+            }
+
+        }
+
+    }
+
+
+}
