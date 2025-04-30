116f7b0937f2797c65502f61c4346dbce228d3d5
Andrew Placencia <sxmxg01@gmail.com>
2025-04-29 21:55:28 -0700

    accidently got rid of statement in this

--- backend/src/main/java/com/team10/demo/controller/AuthenticationController.java
+++ backend/src/main/java/com/team10/demo/controller/AuthenticationController.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.json.JSONObject;
+import java.sql.Statement;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
